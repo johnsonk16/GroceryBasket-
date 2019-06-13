58abb23cd4ff7db262d0fe92486a60d2b21494dd
Francesca <matiraf@wit.edu>
2019-06-13 15:36:07 -0400

    better looking header with sign in/sign up

---
+++ main/js/script.js
@@ -0,0 +1,22 @@
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDSXuuyEHEcBidq2a5RF7tYDoQgvsHPWzs",
+    authDomain: "grocery-basket-web.firebaseapp.com",
+    databaseURL: "https://grocery-basket-web.firebaseio.com",
+    projectId: "grocery-basket-web",
+    storageBucket: "grocery-basket-web.appspot.com",
+    messagingSenderId: "526528269130",
+    appId: "1:526528269130:web:3c565e87471e456a"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+//   // Get the modal
+// var modal = document.getElementById('id01');
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//     if (event.target == modal) {
+//         modal.style.display = "none";
+//     }
+// }
