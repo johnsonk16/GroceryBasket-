8c4c0d6f73daef6bf2b2f119548a877203e11635
Kristin Johnson <johnsonk16@wit.edu>
2019-06-24 12:54:10 -0400

    Update uploadRecipe.php
    Images displayed

--- uploadRecipe.php
+++ uploadRecipe.php
@@ -1,5 +1,5 @@
  <?php
-//have to figure out images but everything sends to DB
+
   require_once('config.php');
 
   $conn = mysqli_connect(DBHOST,DBUSER,DBPASS,DBNAME);
@@ -70,13 +70,14 @@
             if($filesize > $maxsize) die("Error: File size is larger than the allowed limit.");
         
             // Verify MYME type of the file
+
             if(in_array($filetype, $allowed)){
                 // Check whether file exists before uploading it
-                if(file_exists("uploads/" . $filename)){
+                if(file_exists("img/" . $filename)){
                     $filename = $recipeID.$filename;
-                    move_uploaded_file($_FILES["photoUpload"]["tmp_name"], "uploads/" . $filename);
+                    move_uploaded_file($_FILES["photoUpload"]["tmp_name"], "img/" . $filename);
                 } else{
-                    move_uploaded_file($_FILES["photoUpload"]["tmp_name"], "uploads/" . $filename);
+                    move_uploaded_file($_FILES["photoUpload"]["tmp_name"], "img/" . $filename);
                 } 
             } else{
                 echo "Error: There was a problem uploading your file. Please try again."; 
