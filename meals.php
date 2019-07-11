62f6e00b8af20c912fe3e2a5b05dc0b77341e751
Francesca <matiraf@wit.edu>
2019-07-11 11:46:16 -0400

    meals page - still working on it

--- meals.php
+++ meals.php
@@ -24,17 +24,44 @@
      <?php include 'header.php';?>
     </header>
 
-    <div class="cd-intro">
+    <!-- <div class="cd-intro">
 		  <h1>Meals</h1>
 	   </div>
 
-     <!-- 'You have no meals' type of message -->
      <div class="message-no-meals">
        <p>You have no meals saved currently. Start exploring...</p>
        <div class="cd-nugget-info">
           <a href="home.php">Explore</a>
         </div> 
-     </div>
+     </div> -->
+
+    <div class="meals-page"> <!-- meals page below header-->
+      <div class="meals-page__container"> <!-- this is the container wrapper -->
+        <ul class="meals-page__switcher js-meals-page-switcher js-meals-page-trigger">
+          <li><a href="#0" data-signin="meals" data-type="meals">Meals</a></li>
+          <li><a href="#0" data-signin="favorites" data-type="favorites">Favorites</a></li>
+        </ul>
+
+      <div class="meals-page__block js-meals-page-block" data-type="meals"> <!-- meals -->
+        <div class="message-no-meals">
+          <p>You have no meals saved currently. Start exploring...</p>
+          <div class="cd-nugget-info">
+            <a href="home.php">Explore</a>
+          </div> 
+        </div>
+      </div> <!-- meals block -->
+
+      <div class="meals-page__block js-meals-page-block" data-type="favorites"> <!-- favorites -->
+        <div class="message-no-favorites">
+          <p>You have no favorites saved currently. Start exploring...</p>
+          <div class="cd-nugget-info">
+            <a href="home.php">Explore</a>
+          </div> 
+        </div>
+      </div> <!-- favorites block -->
+
+    </div> <!-- meals-page__container -->
+  </div> <!-- meals page below header -->
 
   </body>
 
