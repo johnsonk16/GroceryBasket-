ef69fa848105eb94742293e86c30ed54a0a41f45
Francesca <matiraf@wit.edu>
2019-07-24 16:38:27 -0400

    meals and favorites images

--- meals.php
+++ meals.php
@@ -44,136 +44,130 @@
      <form name = 'ChangeServings' id = 'ChangeServings' action = "basket.php" method='Get'>
             Servings: <input type="text" name="Serving" size="3">
     </form>
+  
 
 
-<div class="meals"> 
-          <?php
+    <div class="meals resize"> 
+      <?php
+      $result = mysqli_query($conn, "SELECT Recipe_ID FROM Meals WHERE User_ID = ".$_SESSION['id']);
+      $numRecipes = mysqli_num_rows($result);
 
-            $result = mysqli_query($conn, "SELECT Recipe_ID FROM Meals WHERE User_ID = ".$_SESSION['id']);
+      if ($numRecipes != 0) {
+        for($i=0; $i<$numRecipes;$i++) { 
+          while($row=mysqli_fetch_array($result, MYSQLI_NUM)) {
+            $RecipeID= $row[0];
 
-            $numRecipes = mysqli_num_rows($result);
+            $sql = "SELECT * FROM Recipes WHERE Recipe_ID = '".$RecipeID. "'";
 
-            if ($numRecipes != 0){
-              for($i=0; $i<$numRecipes;$i++){ 
-                while($row=mysqli_fetch_array($result, MYSQLI_NUM)){
-              $RecipeID= $row[0];
-
-                $sql = "SELECT * FROM Recipes WHERE Recipe_ID = '".$RecipeID. "'";
-
-                $resultR = mysqli_query($conn, $sql);
-                $data = mysqli_fetch_assoc($resultR);
-                $recipeName = $data['Recipe_Name'];
-                $recipeIMG= $data['Recipe_Img'];
+            $resultR = mysqli_query($conn, $sql);
+            $data = mysqli_fetch_assoc($resultR);
+            $recipeName = $data['Recipe_Name'];
+            $recipeIMG= $data['Recipe_Img'];
        
-         if($recipeIMG!="NULL")
-               echo "<img src='img/".$recipeIMG."' id='recipeImage'>";
+            if($recipeIMG!="NULL")
+              echo "<img src='img/".$recipeIMG."' id='recipeImage'>";
        
             else
               echo "<img src='img/GroceryBasket.jpg' id='recipeImage'>";
-               ?>
+            ?>
 
-                </td>
-                </tr>
+            </td>
+            </tr>
 
-                <!-- Recipe Name -->
-                <tr>             
+            <!-- Recipe Name -->
+            <tr>             
 
-                <td colspan="2"><h1>
-
-                <?php 
-           echo "<a href='viewRecipe.php?Recipe_ID=".$RecipeID." '>".$recipeName;
+            <td colspan="2"><h1>
+            <?php 
+              echo "<a href='viewRecipe.php?Recipe_ID=".$RecipeID." '>".$recipeName;
               //  echo $recipeName;
-              ?>
-              <br>
-              <?php
-      } 
-    }   
-       }
-         else {
-               ?> 
-            <div class="message-no-meals"> 
-                    <p>You have no meals saved currently. Start exploring...</p>
-                    <div class="cd-nugget-info">
-                      <a href="home.php">Explore</a>
-                    </div> 
-                  </div>  
-       <?php
-           }       
-              ?>
-
-      </div>
-  </div>
-
-  <div id="favorites" class="tabcontent">
-    <h2>Favorites</h2>
-    <div class="favorites">
- <?php
-            $resultF = mysqli_query($conn, "SELECT Recipe_ID FROM Favorites WHERE User_ID = ".$_SESSION['id']);
-
-            $numRecipesF = mysqli_num_rows($resultF);
-
-            if ($numRecipesF != 0){
-              for($i=0; $i<$numRecipesF;$i++){
-                while($rowF=mysqli_fetch_array($resultF, MYSQLI_NUM)){
-                    $RecipeIDF= $rowF[0];
-                    
-                $sqlF = "SELECT * FROM Recipes WHERE Recipe_ID = '".$RecipeIDF. "'";
-
-                $resultR = mysqli_query($conn, $sqlF);
-                $dataF = mysqli_fetch_assoc($resultR);
-                $recipeNameF = $dataF['Recipe_Name'];
-                $recipeIMGF= $dataF['Recipe_Img'];
-
-         if($recipeIMGF!="NULL")
-                echo "<img src='img/".$recipeIMGF."' id='recipeImage'>";
-       
-                else
-               echo "<img src='img/GroceryBasket.jpg' id='recipeImage'>";
-                 ?>
-
-                </td>
-                </tr>
-
-                <!-- Recipe Name -->
-                <tr>
-
-                <td colspan="2"><h1>
-                <?php 
-           echo "<a href='viewRecipe.php?Recipe_ID=".$RecipeIDF." '>".$recipeNameF;
-           // echo $recipeNameF;
-              }
-         }
-       }
-         else {
-               ?> 
-            <div class="message-no-meals"> 
-                    <p>You have no meals saved currently. Start exploring...</p>
-                    <div class="cd-nugget-info">
-                      <a href="home.php">Explore</a>
-                    </div> 
-                  </div>  
-              <?php
-
-           }       
-              ?>
-
+            ?>
+    
+            <br>
+            <?php
+          } 
+        }   
+      }
+      else {
+        ?> 
+        <div class="message-no-meals"> 
+          <p>You have no meals saved currently. Start exploring...</p>
+          <div class="cd-nugget-info">
+            <a href="home.php">Explore</a>
+          </div> 
+        </div>  
+        <?php
+      }       
+    ?>
     </div>
   </div>
 
-  
+<div id="favorites" class="tabcontent">
+  <h2>Favorites</h2>
+  <div class="favorites resize">
+    <?php
+      $resultF = mysqli_query($conn, "SELECT Recipe_ID FROM Favorites WHERE User_ID = ".$_SESSION['id']);
 
-  <script>
-    function openTab(evt, cityName) {
-      var i, tabcontent, tablinks;
-      tabcontent = document.getElementsByClassName("tabcontent");
-      for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
+      $numRecipesF = mysqli_num_rows($resultF);
+
+      if ($numRecipesF != 0) {
+        for($i=0; $i<$numRecipesF;$i++){
+          while($rowF=mysqli_fetch_array($resultF, MYSQLI_NUM)){
+            $RecipeIDF= $rowF[0];
+                    
+            $sqlF = "SELECT * FROM Recipes WHERE Recipe_ID = '".$RecipeIDF. "'";
+
+            $resultR = mysqli_query($conn, $sqlF);
+            $dataF = mysqli_fetch_assoc($resultR);
+            $recipeNameF = $dataF['Recipe_Name'];
+            $recipeIMGF= $dataF['Recipe_Img'];
+
+            if($recipeIMGF!="NULL")
+              echo "<img src='img/".$recipeIMGF."' id='recipeImage'>";
+            else
+              echo "<img src='img/GroceryBasket.jpg' id='recipeImage'>";
+            ?>
+            </td>
+            </tr>
+            <!-- Recipe Name -->
+            <tr>
+
+            <td colspan="2"><h1>
+            <?php 
+              echo "<a href='viewRecipe.php?Recipe_ID=".$RecipeIDF." '>".$recipeNameF;
+              // echo $recipeNameF;
+          }
+        }
       }
-      tablinks = document.getElementsByClassName("tablinks");
-      for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].className = tablinks[i].className.replace(" active", "");
-      }
-      document.getElementById(cityName).style.display = "block";
+      else {
+        ?> 
+        <div class="message-no-meals"> 
+          <p>You have no meals saved currently. Start exploring...</p>
+          <div class="cd-nugget-info">
+            <a href="home.php">Explore</a>
+          </div> 
+        </div>  
+        <?php
+
+      }       
+    ?>
+
+  </div>
+</div>
+
+  
+<script>
+  function openTab(evt, cityName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
     }
   </script>
